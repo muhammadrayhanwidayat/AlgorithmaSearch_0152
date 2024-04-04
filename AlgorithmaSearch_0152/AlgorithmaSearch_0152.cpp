2b6e32cae12d66f7162bd0fc387e08133068e35c
muhammadrayhanwidayat <murayat99@gmail.com>
2024-04-04 11:07:22 +0700

    membuat pertanyaan bagi pengguna apakah masih ingin melanjutkan pencarian

--- AlgorithmaSearch_0152/AlgorithmaSearch_0152.cpp
+++ AlgorithmaSearch_0152/AlgorithmaSearch_0152.cpp
@@ -50,7 +50,8 @@ void LinearSearch() {
 			cout << "\n" << item << "not found in the array\n";
 		cout << "\nNumber of comparisons: " << ctr << endl;
 
-
+		cout << "\nContinue search(y/n): ";
+		cin >> ch;
 
 	} while ();
 }
