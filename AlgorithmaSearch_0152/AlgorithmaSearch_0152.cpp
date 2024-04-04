4ade14930e5e386a1025998096034a282457d490
muhammadrayhanwidayat <murayat99@gmail.com>
2024-04-04 10:42:15 +0700

    mendeklarasikan variabel

--- AlgorithmaSearch_0152/AlgorithmaSearch_0152.cpp
+++ AlgorithmaSearch_0152/AlgorithmaSearch_0152.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+int arr[20];	//index to be searched
+int n;			//number of elements in the array
+int i;			//index of array element
+
+
 int main()
 {
  
