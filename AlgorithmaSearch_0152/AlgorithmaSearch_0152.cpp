cf4032ac07f9b49d70f24cf3a85247b81e78e22b
muhammadrayhanwidayat <murayat99@gmail.com>
2024-04-04 10:51:06 +0700

    membuat perulangan yang akan membuat array menerima element dari pengguna

--- AlgorithmaSearch_0152/AlgorithmaSearch_0152.cpp
+++ AlgorithmaSearch_0152/AlgorithmaSearch_0152.cpp
@@ -20,6 +20,10 @@ void input() {
 	cout << "\n---------------------------\n";
 	cout << "\n Enter array elements \n";
 	cout << "\n---------------------------\n";
+	for (i = 0; i < n; i++) {
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
 
 }
 
